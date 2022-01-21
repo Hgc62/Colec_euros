4bff02f3987d4c965ef74c3a25f08f2c979c1c16
Hgc62 <74187948+Hgc62@users.noreply.github.com>
2022-01-21 23:40:26 +0100

    Diseño adaptativo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/* Página que muestra las monedas por paises */
+router.get('/monedas', (req, res, next) => {
+  res.render('monedas');
+});
+
 module.exports = router;
